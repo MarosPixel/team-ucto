33b2bef97705dbcce1d9659b79d13597c913d5bc
Maros Pixel <ing.ruso@gmail.com>
2011-11-09 02:45:19 +0100

    oprava 'registration routes'

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,14 @@ TeamUcto::Application.routes.draw do
   
   root to: "home#index"
   resources :users
-  devise_for :user, skip: [:registrations, :sessions] do
+  devise_for :user, skip: [:sessions] do
     get    '/login'               => 'devise/sessions#new',          as:         :new_user_session
     post   '/login'               => 'devise/sessions#create',       as:             :user_session
     get    '/logout'              => 'devise/sessions#destroy',      as:              :logout_user
     delete '/logout'              => 'devise/sessions#destroy',      as:     :destroy_user_session
 
+
+    # pouzit -> skip: [:registrations, :sessions]  
     # get    '/registration'        => 'devise/registrations#new',     as:    :new_user_registration
     # post   '/registration'        => 'devise/registrations#create',  as:        :user_registration
     # get    '/registration/cancel' => 'devise/registrations#cancel',  as: :cancel_user_registration
