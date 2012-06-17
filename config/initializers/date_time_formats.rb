5970ae2f735e031b6c612167bc31ed14caf7fbef
Maros Pixel <ing.ruso@gmail.com>
2012-06-17 10:36:22 +0200

    pridane globalne formatovanie datumu

---
+++ config/initializers/date_time_formats.rb
@@ -0,0 +1,6 @@
+
+# globalny format datum -> 01.02.2000
+Date::DATE_FORMATS[:default] = '%d.%m.%Y'
+
+# globalny format casu -> 01.02.2000 08:00:00
+Time::DATE_FORMATS[:default]= '%d.%m.%Y %H:%M:%S'
