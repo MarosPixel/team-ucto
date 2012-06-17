2d82812b58493e4f4b6ee50c95f71eb332599449
Maros Pixel <ing.ruso@gmail.com>
2012-06-17 10:46:19 +0200

    test imap_mail_processora

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,8 +2,9 @@ class TestController < ApplicationController
   skip_before_filter :authenticate_user!, only: :index
 
   def index
-    @test_content = Mail::download_new_mails()
-    #@test_content = SlspTransList.extract_hb('_podklady/obr-szu.txt')
+    # @test_content = Mail::download_new_mails()
+    @test_content = MailProcessor::download_new_mails
+    # @test_content = SlspTransList.extract_hb('_podklady/obr-szu.txt')
 
     respond_to do |format|
       format.html # index.html.erb
