9828eb8d9b184514f10a89eb53ccd06c06935df8
Maros Pixel <ing.ruso@gmail.com>
2012-06-18 09:38:47 +0200

    pridany transaction processor

--- app/models/transaction_processors/slsp_hb_transaction_processor.rb
+++ app/models/transaction_processors/slsp_hb_transaction_processor.rb
@@ -1,2 +1,61 @@
 class SlspHbTransactionProcessor < TransactionProcessor
+  # Homebanking SLSP
+  # http://www.slsp.sk/downloads/format_hb_vzor.pdf
+
+  def self.extract_all
+    mails = Mails.find_all_by_state(:decoded)
+
+    if mails.nil?
+      # Ziadne volne maily na spracovanie
+    else
+
+      mails.each do |mail|
+        extract(mail)
+      end
+
+    end
+  end
+
+
+  private
+
+    def self.extract(mail)
+      if mail.decoded_attachment.blank?
+        # Nema ziadnu prilohu
+      else
+
+        mail.decoded_attachment.lines.each do |line|
+          pl = line.split(';')
+          if pl.length > 0
+            Transaction.create do |t|
+              t.email_id = mail.id
+              t.datum_transakcie = pl[0]
+              t.predcislo_uctu = pl[1]
+              t.cislo_uctu = pl[2]
+              t.predcislo_protiuctu = pl[3]
+              t.cislo_protiuctu = pl[4]
+              t.kod_banky = pl[5] # neznama premenna
+              t.nazov_protiuctu = pl[6]
+              t.suma = pl[7]
+              t.kod_meny = pl[8]
+              t.konstanta = pl[9]
+              t.zostatok = pl[10]
+              t.nazov_transakcie = pl[11]
+              t.variabilny_symbol = pl[12]
+              t.konstantny_symbol = pl[13]
+              t.specificky_symbol = pl[14]
+              t.indikator_storna = pl[15]
+              t.poznamka = pl[16]
+              t.poradove_cislo_vypisu = pl[17]
+              t.identifikacia_protiuctu = pl[18] + pl[19] + pl[20] + pl[21]
+              t.sprava_pre_prijemcu = pl[22] + pl[23] + pl[24] + pl[25]
+              t.unparsed_transaction = line
+            end
+          end
+
+        end
+
+      end
+    end
+
 end
--- app/models/transaction_processors/transaction_processor.rb
+++ app/models/transaction_processors/transaction_processor.rb
@@ -1,2 +1,13 @@
 class TransactionProcessor
+
+  def self.extract_all
+    transaction_processor::extract_all
+  end
+
+  private
+
+    def transaction_processor
+      APP_CONFIG['processors']['transaction'].constantize
+    end
+
 end
