26771b4dd7b1d8c7511ee1993257cc78e47500f2
Maros Pixel <ing.ruso@gmail.com>
2012-06-18 12:10:46 +0200

    pridany zakladny vypis auditov

--- app/controllers/audits_controller.rb
+++ app/controllers/audits_controller.rb
@@ -1,6 +1,10 @@
 class AuditsController < ApplicationController
   def index
-    @audits = Audit.all
+    @audits = Audit.find_by_sql("SELECT * FROM audits ORDER BY created_at DESC")
+
+    respond_to do |format|
+      format.html # index.html.erb
+    end
   end
 
 end
