4606edd1923c70e68315d0a3d788d7fc2f70636e
Maros Pixel <ing.ruso@gmail.com>
2012-06-23 10:40:02 +0200

    oznacovanie selected aj na deti

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,15 @@
 module ApplicationHelper
-  
+
   public
 
-    def link_to_s(body, url, controller, action = 'index')
+    def link_to_s(body, url, controller, action = false)
       link_to body, url, class: is_selected(controller, action)
     end
 
   private
 
-    def is_selected(controller, action = 'index')
-      'selected' if params['controller'] == controller and params['action'] == action
+    def is_selected(controller, action = false)
+      'selected' if params['controller'] == controller and (!action or params['action'] == action)
     end
 
 end
